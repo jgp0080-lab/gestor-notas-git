77d296d96bdf023015c5389409f6543449ddc3b4
Jesús Gámez <jgp0080@alu.medac.es>
2026-05-14 23:24:44 +0200

    Modificada función de título para permitir personalización de bordes

--- utilidades.py
+++ utilidades.py
@@ -1,2 +1,4 @@
-def mostrar_titulo():
-    print("Gestor de notas en Python")
+def mostrar_titulo(simbolo="*"):
+    print(simbolo * 30)
+    print("  GESTOR DE NOTAS EN PYTHON")
+    print(simbolo * 30)
